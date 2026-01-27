2586cd804fc3ec90b54dcac06f0c1c83090f970f
yufusawa <80983096+yufusawa@users.noreply.github.com>
2026-01-27 10:52:19 +0300

    26.01

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// задача напишите программу  которая запрашивает у пользователя его возраст и выводит сообщение с указанием сколько лет исполнице пользователю через 10 лет
+
+
+
+//  let x = Number(prompt("введите свой возраст"));
+// alert('ваш возраст через 10 лет '+ (x+10));
+
+// let age = prompt( "сколько тебе лет?");
+// age = Number(age)
+// let newAge = age + 10;
+// alert( "вам лет:" + newAge)
+
+
+// let x = + prompt();
+
+// if ( x > 5){    
+//     console.log('x больше 5')
+// }
+// else if( x === 5) { 
+//     console.log( 'x = 5')
+// }
+// else{   
+//     console.log(' x < 5')
+// }
+
+
+// let x = 3;
+// let message = (x>5)?"x больше 5":"x равен 5";
+// console.log(message);
+
+
+// let x = true;
+// let y = false;
+// console.log((x && !y) || (!x && y))
+
+// let number1 = prompt("напишите число с 10 до 100");
+// number1 = Number(number1);
+// if ( number1 >= 10 && number1 <= 100) { 
+//     console.log( "ты молодец число входит в диапозон");
+// }
+// else {      
+//     console.log("число не входит в диапозон");
+// }
+
+
+// let number1 = prompt("напишите число для проверки чётное оно или нет")
+// number1 = Number(number1);
+// if ( number1 % 2 === 0) { 
+//     console.log("число чётное")
+// }
+// else {  
+//     console.log ("число нечётное!")
+// }
+
+// let age = prompt("напишите свой возраст")
+// let message = (age<18)?"доступ запрещён":"доступ разрешён";
+// console.log(message);
