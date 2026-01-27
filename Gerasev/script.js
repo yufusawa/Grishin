d3415ee493fe54f8554241a06d4f8dbca46f79a0
yufusawa <80983096+yufusawa@users.noreply.github.com>
2026-01-27 15:23:38 +0300

    Add files via upload

---
+++ Gerasev/script.js
@@ -0,0 +1,124 @@
+// alert( 'привет мир!!!');
+
+
+
+
+// let message;
+// message = 'hello world!!';  ///////////////    Let переменая для обозначения  слов и цыфр
+// alert(message);
+ 
+
+
+
+// let user = 'шнее пепе';
+// age = '17';
+// alert(user);
+// alert(age);
+
+
+
+ // console.log(1+4+3+6)
+
+//  let user = {   
+// name: "Daniil",
+// age: "17"
+//  };
+//  alert( user.name );
+//  alert( user.age );
+
+
+// delete user.age;
+
+// const user = {
+//   name: "Daniil"
+// };
+
+// user.name = "Pete"; 
+
+// alert(user.name);
+
+
+/*
+        let x=10;
+        let y=5;
+        // let z="first";
+        // let x="second";
+        console.log(x+y);
+        console.log(x-y);
+        console.log(x*y);
+        console.log(x/y);
+        console.log(x**y);
+        console.log(x%y);
+15
+-5 
+50 
+0.5 
+9765625 
+5
+*/
+
+// let age = Number(prompt("Введите свой возраст:"));
+// alert("Ваш возраст через 10 лет: " + (age+10));
+
+
+// let age = prompt("Enter ur age")
+// age=Number(age);
+// let newAge=age+10;
+// alert("ur age in 10 years" + (newAge))
+
+
+// let x=Number(prompt())
+// let x=+prompt()
+// if(x>5){
+//     console.log ("x bolshe 5")}
+// else if (x===5) {
+//     console.log ("x raven 5")
+// }
+// else {
+//     console.log("x menshe 5")
+// }
+// let x=+prompt()
+// let message =(x>5)?"x больше 5": "x меньше или равен 5"
+// console.log(message)
+
+
+// let x=true
+// let y=false
+// console.log((x&&!y)||(!x&&y))
+
+
+
+// 1 ЗАДАЧА
+
+// let n = prompt("напишите число с 10 до 100");
+// n = Number(n);
+// if ( n >= 10 || n <= 100) { 
+//     console.log( "ты молодец число входит в диапозон");
+// }
+// else {      
+//     console.log("число не входит в диапозон");
+// }
+
+// 2 ЗАДАЧА
+
+// let n = prompt("напишите число для проверки чётное оно или нет")
+// n = Number(n);
+// if ( n % 2 === 0) { 
+//     console.log("число чётное")
+// }
+// else {  
+//     console.log ("число нечётное!")
+// }
+
+// 3 ЗАДАЧА
+
+// let age = prompt("напишите свой возраст")
+// let message = (age<18)?"доступ запрещён":"доступ разрешён";
+// console.log(message);
+
+// let  i=0
+// do {
+//         alert(i)
+//         i++
+// } while (i<5)
+
